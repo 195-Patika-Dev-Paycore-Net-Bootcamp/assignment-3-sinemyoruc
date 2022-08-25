427fa5d29d4bf99ba5e4d5e35308b305699b71d6
sinemyoruc <sinemyoruc@gmail.com>
2022-08-25 17:39:14 +0300

    HW3 added

---
+++ SinemYoruc-HW3/SinemYoruc-HW3/Controllers/ContainerController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SinemYoruc_HW3.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class ContainerController : ControllerBase
+    {
+        private readonly IMapperSession session;
+        public ContainerController(IMapperSession session)
+        {
+            this.session = session;
+        }
+
+
+        [HttpGet]
+        public List<Container> Get()
+        {
+            var response = session.Containers.ToList();
+            return response;
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Container container)
+        {
+            try
+            {
+                session.BeginTransaction();
+                session.Save(container);
+                session.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                Log.Error(ex, "Container Insert Error");
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<Container> Put([FromBody] Container request)
+        {
+            Container container = session.Containers.Where(x => x.Id == request.Id).FirstOrDefault();
+            if (container == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                session.BeginTransaction();
+
+                container.ContainerName = request.ContainerName;
+                container.Latitude = request.Latitude;
+                container.Longitude = request.Longitude;
+
+                session.Update(container);
+
+                session.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                Log.Error(ex, "Container Updated Error");
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Container> Delete(int id)
+        {
+            Container container = session.Containers.Where(x => x.Id == id).FirstOrDefault();
+            if (container == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                session.BeginTransaction();
+                session.Delete(container);
+                session.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                Log.Error(ex, "Container Deleted Error");
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ SinemYoruc-HW3/SinemYoruc-HW3/Controllers/VehicleController.cs
@@ -0,0 +1,138 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SinemYoruc_HW3
+{
+    [ApiController]
+    [Route("api/[controller]s")]
+    public class VehicleController : ControllerBase
+    {
+        private readonly IMapperSession session;
+        public VehicleController(IMapperSession session)
+        {
+            this.session = session;
+        }
+
+
+        [HttpGet]
+        public List<Vehicle> Get()
+        {
+            var response = session.Vehicles.ToList();
+            return response;
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<List<Container>> GetById(int id)
+        {
+            try
+            {
+                Vehicle vehicle = session.Vehicles.Where(x => x.Id == id).FirstOrDefault();
+                var container = session.Containers.Where(x => x.VehicleId == vehicle.Id).ToList();
+                var response = session.Containers.Where(x => x.VehicleId == vehicle.Id).ToList();
+                return response;
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Vehicle is not found");
+                return NotFound("Vehicle is not found");
+            }
+
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Vehicle vehicle)
+        {
+            try
+            {
+                session.BeginTransaction();
+                session.Save(vehicle);
+                session.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                Log.Error(ex, "Vehicle Insert Error");
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<Vehicle> Put([FromBody] Vehicle request)
+        {
+            Vehicle vehicle = session.Vehicles.Where(x => x.Id == request.Id).FirstOrDefault();
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                session.BeginTransaction();
+
+                vehicle.VehicleName = request.VehicleName;
+                vehicle.VehiclePlate = request.VehiclePlate;
+
+                session.Update(vehicle);
+
+                session.Commit();
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                Log.Error(ex, "Vehicle Updated Error");
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<Vehicle> Delete(int id)
+        {
+            Vehicle vehicle = session.Vehicles.Where(x => x.Id == id).FirstOrDefault();
+            
+            if (vehicle == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                session.BeginTransaction();
+                session.Delete(vehicle);
+                session.Commit();
+                Container container = session.Containers.Where(x => x.VehicleId == vehicle.Id).FirstOrDefault();
+                if (container != null)
+                {
+                    if (vehicle.Id == container.VehicleId)
+                    {
+                        session.Delete(container);
+                        session.Commit();
+                    }
+                } 
+            }
+            catch (Exception ex)
+            {
+                session.Rollback();
+                Log.Error(ex, "Vehicle Deleted Error");
+            }
+            finally
+            {
+                session.CloseTransaction();
+            }
+
+            return Ok();
+        }
+
+    }
+}
